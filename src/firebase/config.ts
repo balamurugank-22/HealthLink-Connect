48d5f65fe4bb44306134ee53e9c11a41c85afcd5
BALAMURUGAN K <kbalamurugana725@gmail.com>
2026-02-27 06:40:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1658505467-c096e",
+  "appId": "1:944501401513:web:e15f5aef3f19d9be5946b8",
+  "apiKey": "AIzaSyCkDwvoAz8Dd0G9osJoIP_L8Ma9JM9vgd4",
+  "authDomain": "studio-1658505467-c096e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "944501401513"
+};
